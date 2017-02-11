e7d35cb64c51986f7725dfa8546ccdcfce11b340
joshkmcdonald <joshkmcdonald@gmail.com>
2017-02-10 18:38:17 -0700

    Merged branches and fixed bugs

--- app/models/room_event.rb
+++ app/models/room_event.rb
@@ -1,17 +1,20 @@
 class RoomEvent < ApplicationRecord
 	validate :correct_times
 	validate :booked
+  belongs_to :room
+  belongs_to :user
+  scope :today, -> { where(start_time: Date.today.beginning_of_day..Date.today.end_of_day)}
 
 	def booked
-		if RoomEvent.where(start_time: self.start_time..self.end_time, end_time: self.start_time..self.end_time).any?
+		if RoomEvent.where(room_id: self.room_id).where("start_time BETWEEN ? AND ? OR end_time BETWEEN ? and ?", self.start_time, self.end_time, self.start_time, self.end_time).any?
 			self.errors.add(:booked, 'Already booked')
 			false
 		end
 	end
 
   def correct_times
-    if self.end_time && (self.end_time > self.start_time)
-      self.errors.add(:time_error, 'Incorrect time zones')
+    if self.end_time && (self.end_time < self.start_time)
+      self.errors.add(:time_error, 'End time can\' be before start time')
       false
     end
   end
